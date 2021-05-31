d1e07fa6ff0a6e91abe17d2fd79b74beac7569c3
Hal Mueller <halmueller@gmail.com>
2021-05-30 17:16:17 -0700

    Add template project.

---
+++ iOS project/Rad Wookie Movies/Rad Wookie Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rad Wookie Movies
+//
+//  Created by Hal Mueller on 5/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
