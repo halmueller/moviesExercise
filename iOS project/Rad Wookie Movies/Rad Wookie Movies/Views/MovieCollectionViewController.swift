e3f1f01f0aace573f6bd9b9e2ec58b0ec3e614d4
Hal Mueller <halmueller@gmail.com>
2021-06-06 20:52:43 -0700

    Less cruft.

--- iOS project/Rad Wookie Movies/Rad Wookie Movies/Views/MovieCollectionViewController.swift
+++ iOS project/Rad Wookie Movies/Rad Wookie Movies/Views/MovieCollectionViewController.swift
@@ -116,38 +116,14 @@ class MovieCollectionViewController: UICollectionViewController, UICollectionVie
 
     // MARK: - UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        print(#function, indexPath)
         return true
     }
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(#function, indexPath)
         selectedMovie = movie(indexPath: indexPath)
         performSegue(withIdentifier: "showMovieDetail", sender: self)
     }
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
     // MARK: - UICollectionViewDelegateFlowLayout
     
